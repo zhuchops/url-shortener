04a834f90eeba9dbd9cdc887861c2c4cf8114e2c
zhuchok <zhuchok@duck.com>
2026-04-14 10:40:24 +0300

    first commit

---
+++ rust-server/src/main.rs
@@ -0,0 +1,20 @@
+use axum::{Router, routing::{get, post}};
+
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root))
+        .route("/health", get(health));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    _ = axum::serve(listener, app).await;
+}
+
+async fn root() -> &'static str {
+    return "Hello world"
+}
+
+async fn health() -> &'static str {
+    return "I'm OK"
+}
