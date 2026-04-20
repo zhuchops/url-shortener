c941ef17e69986157fb68810c54bd012225ce5db
zhuchops <zhuchok@duck.com>
2026-04-20 12:20:00 +0300

    moved db logic to separate file

--- rust-server/src/main.rs
+++ rust-server/src/main.rs
@@ -2,13 +2,17 @@ mod db;
 
 use std::{default, env};
 
-use axum::{Router, extract::{Path, Query, State}, http::StatusCode, routing::{get, post}};
+use axum::{
+    Router,
+    extract::{Path, Query, State},
+    http::StatusCode,
+    routing::{get, post},
+};
 
 use serde::Deserialize;
 use sqlx::PgPool;
 
 use crate::db::{Db, DbError};
-
 
 #[tokio::main]
 async fn main() {
@@ -16,7 +20,7 @@ async fn main() {
     let db = Db::new(db_url).await;
     let app = Router::new()
         .route("/short", post(short))
-        .route("/get/", get(get_url))
+        .route("/get/{id}", get(get_url))
         .with_state(db)
         .route("/", get(root))
         .route("/health", get(health));
@@ -26,23 +30,19 @@ async fn main() {
 }
 
 async fn root() -> String {
-    return "Hello world".to_string()
+    return "Hello world".to_string();
 }
 
 async fn health() -> StatusCode {
-    return StatusCode::OK
+    return StatusCode::OK;
 }
-
 
 #[derive(Deserialize)]
-struct GetOrPostUrl {
-    link: String
+struct PostUrl {
+    link: String,
 }
 
-async fn short(
-    Query(params): Query<GetOrPostUrl>,
-    State(db): State<Db>
-) -> Result<String, StatusCode> {
+async fn short(Query(params): Query<PostUrl>, State(db): State<Db>) -> Result<String, StatusCode> {
     let true_url = params.link;
     let shorted_url_result = db.short_link(true_url);
     match shorted_url_result.await {
@@ -50,17 +50,17 @@ async fn short(
         Err(err) => match err {
             DbError::AlreadyExists => Err(StatusCode::CONFLICT),
             _ => Err(StatusCode::INTERNAL_SERVER_ERROR),
-        }
+        },
     }
 }
 
-async fn get_url(State(db): State<Db>, Path(url): Path<String>) -> Result<String, StatusCode> {
-    let true_url_result = db.get_link(url).await;
+async fn get_url(State(db): State<Db>, Path(id): Path<String>) -> Result<String, StatusCode> {
+    let true_url_result = db.get_link(id).await;
     match true_url_result {
         Ok(true_url) => Ok(true_url),
         Err(err) => match err {
             DbError::NoSuchLink => Err(StatusCode::NOT_FOUND),
-            _ => Err(StatusCode::INTERNAL_SERVER_ERROR)
-        }
+            _ => Err(StatusCode::INTERNAL_SERVER_ERROR),
+        },
     }
 }
